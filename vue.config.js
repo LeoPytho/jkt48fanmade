fbd2eceeadb7ef8a82b6d3aa71af0923b7badb90
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-07-05 17:03:19 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,20 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist',
   assetsDir: 'static',
+  publicPath: '/',
   devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src')
       }
     }
   }
